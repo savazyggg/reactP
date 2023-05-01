abfe799a5fe53e7219664660f969054e553056b6
savazyggg <younji0715@gmail.com>
2023-05-01 23:52:43 +0900

    react practic

---
+++ react/dice_game/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import Board from './Board';
+import Button from './button';
+import './app.css'
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
+
+function App() {
+const [myHistory, setMyHistory] = useState([]);
+const [otherHistory, setOtherHistory] = useState([]);
+
+  const handleRollClick = () => {
+    const nextMyNum = random(6);
+    const nextOtherNum = random(6);
+    setMyHistory([...myHistory, nextMyNum]);
+    setOtherHistory([...otherHistory, nextOtherNum]);
+  };
+
+  const handleClearClick = () => {
+   setMyHistory([]);
+   setOtherHistory([]);
+  };
+
+  return (
+    <div className='App'>
+      <Button className='App-button' color="blue" onClick={handleRollClick}>던지기</Button>
+      <Button className='App-button' color="red" onClick={handleClearClick}>처음부터</Button>
+      <div>
+        <Board name="나" color="blue" gameHistory={myHistory} />
+        <Board name="상대" color="red" gameHistory={otherHistory} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/example/src/components/App.js
@@ -0,0 +1,28 @@
+import ReviewList from "./ReviewList";
+import mockItems from "../mock.json";
+import { useState } from "react";
+
+function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOder("createdAt");
+  const handleBestClick = () => setOder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+    </div>
+  );
+}
+
+export default App;
