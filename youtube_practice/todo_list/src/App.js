df8131aa4754001eb896edddf80987dd530827df
savazyggg <younji0715@gmail.com>
2023-05-08 00:48:44 +0900

    todolist update

---
+++ youtube_practice/todo_list/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Header from "./components/header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "ji", day: "12/3", reminder: true },
+    { id: 2, text: "lala", day: "11/10", reminder: true },
+    { id: 3, text: "hi", day: "11/11", reminder: true },
+  ]);
+
+  //Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
+  //delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
+    </div>
+  );
+}
+
+export default App;
