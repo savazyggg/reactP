af7468fb018bd3a538b520a6682db29ffdcf828c
savazyggg <younji0715@gmail.com>
2023-05-14 03:45:51 +0900

    youtube

---
+++ youtube_practice/modernWeb3/youtube/src/components/SearchBar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
+  return (
+    <Paper
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 }, //sx는 only for style이고 sx안에서 약어 사용가능/특정 스타일을 지정할 수 있다.
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
